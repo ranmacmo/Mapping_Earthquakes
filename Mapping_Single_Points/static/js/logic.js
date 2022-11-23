ae5f5ecb0f1ee713c2d34ffb66b9a2aefd677a4e
ranmacmo <rmachacek@icloud.com>
2022-11-23 11:52:20 -0600

    Single Point Commit

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,8 +1,8 @@
 // Create our initial map object
 // Set the longitude, latitude, and the starting zoom level
 var myMap = L.map("map", {
-  center: [40.7, -94.5],
-  zoom: 4
+  center: [34.0522, -118.2437],
+  zoom: 14
 });
 
 // Add a tile layer (the background map image) to our map
@@ -13,6 +13,17 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: 'mapbox/dark-v10',
   accessToken: API_KEY
 }).addTo(myMap)
+
+//  Add a marker to the map for Los Angeles, California.
+L.marker([34.0522, -118.2437]).addTo(myMap);
+
+// Add a circle to the map with radius of 100
+L.circle([34.0522, -118.2437], {
+   radius: 300,
+   color: 'black',
+   fill: true,
+   fillColor : 'yellow'
+}).addTo(myMap);
