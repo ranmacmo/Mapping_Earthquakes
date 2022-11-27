5c1271bbb3e1eeb63a2f7854d1210666e842472d
ranmacmo <rmachacek@icloud.com>
2022-11-27 15:52:24 -0600

    Add circles

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -45,10 +45,72 @@ let myStyle = {
     weight: 2
 }
 
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
 // Grabbing our GeoJSON data.
+// d3.json(eqData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data).addTo(myMap);
+// });
+
+
+// ** Grabbing our GeoJSON data
+// d3.json(eqData).then(function(data) {
+//     console.log(data)
+//   // Creating a GeoJSON layer with retrieved data
+//   L.geoJson(data, {
+//     // style:myStyle,
+//     pointToLayer : function(feature, latlng){
+//         console.log(latlng)
+//         return L.circleMarker(latlng)
+//          }
+//   }).addTo(myMap);
+// });
+
+
+// ** Grabbing our GeoJSON data
 d3.json(eqData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(myMap);
+    console.log(data)
+  // Creating a GeoJSON layer with retrieved data
+  L.geoJSON(data, {
+    // We turn each feature into a circleMarker on the map.
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+      // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo
+    }).addTo(myMap);
 });
+
+
+
+
+
+
+
+
 
