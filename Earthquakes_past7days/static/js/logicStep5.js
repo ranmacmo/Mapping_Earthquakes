1c6eee730fbb45a388803027a3fd98ceddf66863
ranmacmo <rmachacek@icloud.com>
2022-11-27 17:03:13 -0600

    css lengend

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -37,7 +37,7 @@ let baseMaps = {
 };
 
 
-// ADD EARTHQUAKE LAYER ----------
+// ADD EARTHQUAKE LAYER ---------
 
 // Create the earthquake layer for our map.
 let earthquakes = new L.layerGroup();
@@ -54,6 +54,40 @@ L.control.layers(baseMaps, overlays).addTo(myMap);
 
 // END EARTHQUAKE LAYER ----------
 
+// CREATE LEGEND
+// Create Constants
+const magnitudes = [0, 1, 2, 3, 4, 5];
+const colors = [
+  "#98ee00",
+  "#d4ee00",
+  "#eecc00",
+  "#ee9c00",
+  "#ea822c",
+  "#ea2c2c"
+];
+
+// 1. Create a legend control object.
+let legend = L.control({
+  position: "bottomright"
+});
+
+// Then add all the details for the legend.
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  // Looping through our intervals to generate a label with a colored square for each interval.
+   for (var i = 0; i < magnitudes.length; i++) {
+     console.log(colors[i]);
+     div.innerHTML +=
+       "<i style='background: " + colors[i] + "'></i> " +
+       magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+  }
+   return div;
+
+};
+
+legend.addTo(myMap);
+
+// END CREATE LEGEND
 
 // Create a style for the lines.
 let myStyle = {
